06793d556dc00cfe6ce1cbeeeae3179e9a2437f1
Madhu Soodhanan <a.madhusoodhanan1996@gmail.com>
2024-02-15 19:34:50 +0530

    Changes to Access Steam API And Refactoring Project Files

---
+++ services/steam_helper.py
@@ -0,0 +1,34 @@
+import requests
+import json
+
+API_KEY: str = ''
+BASE_URL: str = ''
+
+with open('../config_keys.json') as config_file:
+    config_data = json.load(config_file)
+    steam_values = config_data['steam']
+    API_KEY = steam_values["API_KEY"]
+    BASE_URL = steam_values["BASE_URL"]
+
+
+def get_game_list():
+    """
+    Get the player summary for a given Steam ID.
+    """
+    url = f"{BASE_URL}/ISteamApps/GetAppList/v0002/?key={API_KEY}&format=json"
+    response = requests.get(url)
+    data = response.json()
+    return data
+
+
+def get_player_summary(steam_id):
+    """
+    Get the player summary for a given Steam ID.
+    """
+    url = f"{BASE_URL}/ISteamUser/GetPlayerSummaries/v0002/?key={API_KEY}&steamids={steam_id}&format=json"
+    response = requests.get(url)
+    data = response.json()
+    return data
+
+# Example usage
+# entire_game_list = list(get_game_list()["applist"]["apps"])
